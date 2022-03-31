ccc64124400b8a3c3fbe4a75367827b73065e603
LieTu1003 <1278496262@qq.com>
2022-03-31 09:25:56 +0000

    新增类，扩展类，mixins

--- 01_dart_start.dart
+++ 01_dart_start.dart
@@ -1,6 +1,9 @@
-//变量(https://dart.cn/samples#variables)
+//代码写完后需要进行规范化 format document
+//导入(https://dart.cn/samples#imports)
+import 'dart:math';
 import 'dart:io';
 
+//变量(https://dart.cn/samples#variables)
 void variables() {
   var name = 'Voyager I';
   var year = 1977;
@@ -39,15 +42,58 @@ void control() {
     year += 1;
   }
 }
+
 //函数(https://dart.cn/samples#functions)
 int fibonacci(int n) {
   if (n == 0 || n == 1) return n;
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
+//类(https://dart.cn/samples#classes)
+class Spacecraft {
+  String name;
+  DateTime? launchDate;
+  int? get launchYear => launchDate?.year;
+  Spacecraft(this.name, this.launchDate) {}
+  Spacecraft.unlaunched(String name) : this(name, null);
+  void describe() {
+    print('Spacecraft:$name');
+    var launchDate = this.launchDate;
+    if (launchDate != null) {
+      int years = DateTime.now().difference(launchDate).inDays ~/ 365;
+      print('Launched:$launchYear($years years ago)');
+    } else {
+      print('Unlaunched');
+    }
+  }
+}
+
+//扩展类(https://dart.cn/samples#inheritance)
+class Orbiter extends Spacecraft {
+  double altitute;
+  Orbiter(String name, DateTime launchDate, this.altitute)
+      : super(name, launchDate);
+}
+
+//Mixins(https://dart.cn/samples#mixins)
+mixin Piloted {
+  int astronauts = 1;
+
+  void describeCrew() {
+    print('Number of astronauts: $astronauts');
+  }
+}
+
 void main(List<String> args) async {
+  //变量
   variables();
+  //流程控制语句
   control();
+  //函数
   var result = fibonacci(20);
   print(result);
+  //类的调用
+  var voyager = Spacecraft("Voyager Ⅰ", DateTime(1977, 9, 5));
+  voyager.describe();
+  var voyager3 = Spacecraft.unlaunched('Voyager Ⅲ');
 }
