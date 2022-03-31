a1edc50e65751c1e2c5945a727183bb81c41fdbe
LieTu1003 <1278496262@qq.com>
2022-03-31 08:40:04 +0000

    新增流程控制，函数

--- 01_dart_start.dart
+++ 01_dart_start.dart
@@ -14,6 +14,40 @@ void variables() {
       "name is $name,year is $year,antennaDiameter is $antennaDiameter,flybyObjects is $flybyObjects");
 }
 
+//流程控制语句(https://dart.cn/samples#control-flow-statements)
+void control() {
+  var name = 'Voyager I';
+  var year = 1977;
+  var antennaDiameter = 3.7;
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  var image = {
+    'tags': ['saturn'],
+    'url': "//path/to/saturn.jpg"
+  };
+  if (year >= 2001) {
+    print('21st century');
+  } else if (year >= 1901) {
+    print('20th century');
+  }
+  for (final object in flybyObjects) {
+    print(object);
+  }
+  for (int month = 1; month <= 12; month++) {
+    print(month);
+  }
+  while (year < 2016) {
+    year += 1;
+  }
+}
+//函数(https://dart.cn/samples#functions)
+int fibonacci(int n) {
+  if (n == 0 || n == 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
 void main(List<String> args) async {
   variables();
+  control();
+  var result = fibonacci(20);
+  print(result);
 }
